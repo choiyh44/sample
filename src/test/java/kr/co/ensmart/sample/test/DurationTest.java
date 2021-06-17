16a3ee784883baa007a115315ba6cbe51b6ab411
choiyh <choiyh44@gmail.com>
2021-06-17 17:51:57 +0900

    DurationTest 작성

---
+++ src/test/java/kr/co/ensmart/sample/test/DurationTest.java
@@ -0,0 +1,18 @@
+package kr.co.ensmart.sample.test;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class DurationTest {
+
+	@Test
+	void test() {
+		log.debug("Duration.ofSeconds(2): {}", Duration.ofSeconds(2));
+		log.debug("Duration.ofSeconds(2).toSeconds(): {}", Duration.ofSeconds(2).toSeconds());
+	}
+
+}
